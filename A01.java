60c75f4fffdb70095188c7e63f3cecb08c46aa54
RReed2015 <rreed54@bruinmail.slcc.edu>
2025-01-23 14:06:44 -0700

    Add files via upload

---
+++ A01.java
@@ -0,0 +1,16 @@
+/**
+ *Assignment: A01 - Print
+ *
+ *@author: Robert Reed
+ */
+
+
+public class A01 {
+   public static void main(String[] args) {
+      System.out.println(" _[@]_     /\\/\\ ");
+      System.out.println("  (\")     (>''>)");
+      System.out.println(" (_:_)    ( UU )");
+      
+      URL myURL = new URL("http://RobertReedA01.com/");
+   }
+}
